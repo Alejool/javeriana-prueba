7522fe3a751b823c36e71bb908c7a41ab03df200
Alejandro Olarte <olartealejandro48@gmail.com>
2026-04-25 18:28:20 -0500

    style: update logo in header, footer and favicon

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -1,16 +1,16 @@
-import { GraduationCap } from 'lucide-react';
-
 export const Footer = () => {
   return (
     <footer className="bg-primary dark:bg-primary text-white mt-16 py-8 border-t-4 border-secondary-500">
       <div className="container mx-auto px-4">
         <div className="text-center">
-          <div className="flex items-center justify-center gap-2 mb-4">
-            <GraduationCap className="w-6 h-6 text-secondary-400" />
-            <span className="font-headline font-bold text-xl">Universidad Javeriana</span>
+          <div className="flex items-center justify-center  gap-2 mb-4">
+            <img src="/favicon.svg" alt="Logo" className="w-6 h-6" />
+            <span className="font-headline font-bold text-xl">
+              Pontificia Universidad Javeriana
+            </span>
           </div>
           <p className="text-sm text-primary-100 font-body">
-            © 2026 Prueba técnica  
+            © 2026 Prueba técnica
           </p>
         </div>
       </div>
--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,5 +1,4 @@
-import { GraduationCap } from 'lucide-react';
-import { DarkModeToggle } from '@/shared/components/DarkModeToggle';
+import { DarkModeToggle } from "@/shared/components/DarkModeToggle";
 
 export const Header = () => {
   return (
@@ -8,7 +7,11 @@ export const Header = () => {
         <div className="flex items-start sm:items-center justify-between gap-4">
           <div className="flex items-center gap-3 sm:gap-4">
             <div className="p-2 sm:p-3 shrink-0">
-              <GraduationCap className="w-8 h-8 sm:w-10 sm:h-10 text-white" />
+              <img
+                src="/favicon.svg"
+                alt="Logo"
+                className="w-8 h-8 sm:w-10 sm:h-10"
+              />
             </div>
             <div>
               <h1 className="text-xl sm:text-2xl md:text-3xl font-headline font-bold leading-tight">
